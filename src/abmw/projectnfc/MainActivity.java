a9654f3c34e017819c7e2348c04ca1c345537845
Michael Walter <mw@mytransoffice.de>
2012-09-11 21:27:38 +0200

    11.09.2012

--- src/abmw/projectnfc/MainActivity.java
+++ src/abmw/projectnfc/MainActivity.java
@@ -2,7 +2,9 @@ package abmw.projectnfc;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,9 +12,19 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        String x = "Andrej";
+        
         
         setContentView(R.layout.activity_main);
+        
+        Bundle extras = getIntent().getExtras();
+        
+        if (extras != null) {
+	        String value1 = extras.getString(Intent.EXTRA_TEXT);
+	        
+	        if (value1 != null) {
+	        	TextView tv = (TextView) findViewById(R.id.txMain);
+	        	tv.setText(value1);
+        }}
     }
 
     @Override
